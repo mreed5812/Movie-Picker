596b0a75e7412683faf35561b860d13b5b4e7f09
Michael Reed <mreed5812@gmail.com>
2020-11-19 07:09:58 -0500

    updated csv creation to use csv lib for cleaner function

--- Netflix.py
+++ Netflix.py
@@ -1,6 +1,7 @@
 import requests
 import pandas
 from bs4 import BeautifulSoup
+import csv
 
 #URL = 'https://www.monster.com/jobs/search/?q=Programmer&where=New-York'
 URL = 'https://www.netflix.com/browse/genre/34399'
@@ -13,9 +14,12 @@ movie_rows = soup.findAll('span', {"class": "nm-collections-title-name"})
 
 movie_list = []
 
+
 for row in movie_rows:
     movie_list.append(row.text)
-    print(row.text)
+   # print(row.text)
 
-pd = pandas.DataFrame(movie_list)
-pd.to_csv("movies.csv")
+
+with open("out.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(movie_list)
